ea447015011e8e6c2307bb54de5f9f10b6a7f22b
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-07-26 10:43:31 +0600

    feat: configure Prettier and integrate with ESLint

--- templates/ts-esm-feature/eslint.config.js
+++ templates/ts-esm-feature/eslint.config.js
@@ -1,29 +1,34 @@
-import eslint from '@eslint/js';
+import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default tseslint.config(
-  eslint.configs.recommended,
+export default defineConfig([
+  js.configs.recommended,
   {
     ignores: ['dist/', 'node_modules/'],
   },
   {
-    files: ['src/**/*.ts'],
-    extends: [
-      tseslint.configs.recommended,
-      tseslint.configs.recommendedTypeChecked,
-    ],
     languageOptions: {
       globals: {
         ...globals.node,
       },
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
     },
   },
   {
+    files: ['src/**/*.ts'],
+    extends: tseslint.configs.recommendedTypeChecked.map((config) => ({
+      ...config,
+      files: ['src/**/*.ts'],
+    })),
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
     rules: {},
-  }
-);
+  },
+  eslintConfigPrettier,
+]);
