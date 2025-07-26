a3fdc0c0ad752af1002189a41a321d464d6e76b0
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-07-26 12:25:05 +0600

    feat: implement conditional logic for language choice

--- src/commands/createProject.js
+++ src/commands/createProject.js
@@ -29,11 +29,20 @@ export const createProject = async (projectName, options) => {
       ],
     });
 
-    console.log(`Test prompt: ${language}`);
+    console.log(
+      chalk.blue(
+        `✨ Scaffolding a ${
+          language === 'ts' ? 'TypeScript' : 'JavaScript'
+        } project...`
+      )
+    );
 
     const projectPath = path.join(process.cwd(), projectName);
-    const rootPath = path.resolve(options.dirname, '../../'); // Go up from src/commands to root
-    const templatePath = path.join(rootPath, 'templates', 'ts-esm-feature');
+    const rootPath = path.resolve(options.dirname, '../../');
+
+    // Determine which template to use based on the user's choice
+    const templateDir = `templates/${language}-esm-feature`;
+    const templatePath = path.join(rootPath, templateDir);
 
     // Copy template files
     fs.copySync(templatePath, projectPath, {
@@ -57,7 +66,7 @@ export const createProject = async (projectName, options) => {
       newPackageJsonContent
     );
 
-    console.log(chalk.green.bold('Project scaffolded successfully!'));
+    console.log(chalk.green.bold('\n✅Project scaffolded successfully!'));
     console.log(`\nNext steps:`);
     console.log(chalk.yellow(`  cd ${projectName}`));
     console.log(chalk.yellow(`  npm install`));
